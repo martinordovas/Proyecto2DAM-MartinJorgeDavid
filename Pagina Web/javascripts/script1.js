4a825bb4e29b4b58722a31104b9917bb0e762771
martinordovas <martinordovas@gmail.com>
2025-10-16 12:35:22 +0200

    Cambios en la estructura y página Empresas añadida

--- Pagina Web/javascripts/script1.js
+++ Pagina Web/javascripts/script1.js
@@ -1,5 +1,13 @@
 const barraNavegacion = document.getElementById('navbarPaginaPrincipal');
+const contenidoMenu = document.getElementById('menuNavbar');
 const altura = 50;
+
+function comprobarScroll() {
+    if (contenidoMenu.classList.contains('show')){
+        return;
+    }
+}
+
 window.addEventListener('scroll', function() {
     if (barraNavegacion && window.scrollY > altura) {
         barraNavegacion.classList.add('cambioNavbar');
@@ -7,3 +15,15 @@ window.addEventListener('scroll', function() {
         barraNavegacion.classList.remove('cambioNavbar');
     }
 });
+
+contenidoMenu.addEventListener('show.bs.collapse', function() {
+    barraNavegacion.classList.add('cambioNavbar');
+});
+
+contenidoMenu.addEventListener('hide.bs.collapse', function() {
+    if (window.scrollY <= altura) {
+        barraNavegacion.classList.remove('cambioNavbar');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', comprobarScroll);
