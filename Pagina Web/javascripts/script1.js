22162be0e6c2ed2435b18c844dc065d6053c9d2a
martinordovas <martinordovas@gmail.com>
2025-10-18 12:17:56 +0200

    Cambios generales

--- Pagina Web/javascripts/script1.js
+++ Pagina Web/javascripts/script1.js
@@ -6,7 +6,7 @@ const contenidoMenu = document.getElementById('menuNavbar');
 const quienesSomos = document.getElementById('quienesSomos');
 const altura = 50;
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
     if (barraNavegacion && window.scrollY > altura) {
         barraNavegacion.classList.add('cambioNavbar');
     } else {
@@ -16,26 +16,26 @@ window.addEventListener('scroll', function() {
     }
 });
 
-contenidoMenu.addEventListener('show.bs.collapse', function() {
+contenidoMenu.addEventListener('show.bs.collapse', function () {
     barraNavegacion.classList.add('cambioNavbar');
 });
 
-contenidoMenu.addEventListener('hide.bs.collapse', function() {
+contenidoMenu.addEventListener('hide.bs.collapse', function () {
     if (window.scrollY <= altura) {
         barraNavegacion.classList.remove('cambioNavbar');
     }
 });
 
-function aparecerRecuadro(nombre, altura){
+function aparecerRecuadro(nombre, altura) {
     const recuadro = document.getElementById(nombre);
-    if(!recuadro)return;
-    const opciones = {root: null, rootMargin: `0px 0px ${altura}px 0px`, threshold: 0.1};
+    if (!recuadro) return;
+    const opciones = { root: null, rootMargin: `0px 0px ${altura}px 0px`, threshold: 0.1 };
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.remove('ocultar'); 
+                entry.target.classList.remove('ocultar');
                 entry.target.classList.add('aparecer');
-                
+
                 observer.unobserve(entry.target);
             }
         });
